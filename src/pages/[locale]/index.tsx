090812abcd8fb6fec96d0881a65afbe0ed868929
yu <yu@yu--MacBook-Air.local>
2024-01-21 21:57:27 +0900

    불필요 코드 정리

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -1,10 +1,21 @@
+import LocaleLink from "@/components/LocaleLink";
 import { getStaticPaths, makeStaticProps } from "@/utils/getStatic";
 import { useRouter } from "next/router";
 
 export const LocaleIndexPage = () => {
   const router = useRouter();
 
-  return <div>{router.asPath}</div>;
+  return (
+    <div>
+      <div>{router.asPath}</div>
+      <div>
+        <LocaleLink href="/test1">페이지1이동</LocaleLink>
+      </div>
+      <div>
+        <LocaleLink href="/test2">페이지2이동</LocaleLink>
+      </div>
+    </div>
+  );
 };
 
 export default LocaleIndexPage;
